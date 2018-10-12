5991abfdc97fd625c1288f3dacfef1ad30943713
Rennzh <hennzr@gmail.com>
2018-10-12 10:52:59 +0800

    (FEATURE)[SDK-IOS]完善神策IOS-SDK的封装

--- ios/RNSensorsAnalytics.h
+++ ios/RNSensorsAnalytics.h
@@ -1,5 +1,14 @@
+//
+//  RNSensorsAnalytics.h
+//  hunheDemo
+//
+//  Created by 肖彦敏 on 2017/4/14.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
 
-@interface RNSensorsAnalytics : NSObject <RCTBridgeModule>
+@interface RNSensorsAnalytics : NSObject<RCTBridgeModule>
 
 @end
